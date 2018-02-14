7e979a4b0265f1d14e5892fc63e9c4925070de0b
bhombalziyad20 <zeebhombal@gmail.com>
2018-02-14 13:35:04 +0530

    Commit #2

--- src/main/java/ZiyadBhombal/App.java
+++ src/main/java/ZiyadBhombal/App.java
@@ -1,63 +1,95 @@
 package ZiyadBhombal;
 import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import mdlaf.*;
 import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.text.Document;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.io.File;
 import java.io.FileNotFoundException;
 
 /**
  * Hello world!
  *
  */
-public class App 
+@SuppressWarnings("ALL")
+public class App
 {
-    static PdfWriter writer;
-    public static void main( String[] args )
+    static File pdfFile;
+    public static void main( String[] args ) throws Exception
     {
-        try{
-            UIManager.setLookAndFeel(new MaterialLookAndFeel());
-        }
-        catch (UnsupportedLookAndFeelException e){
-            e.printStackTrace();
-        }
+        EventQueue.invokeAndWait(new Runnable() {
+            public void run() {
+//                try{
+//                    UIManager.setLookAndFeel(new MaterialLookAndFeel());
+//                }
+//                catch (UnsupportedLookAndFeelException e){
+//                    e.printStackTrace();
+//                }
 
-        JFrame frame = new JFrame("Test Window");
-        frame.setMinimumSize(new Dimension(600,400));
+                final JFrame frame = new JFrame("Test Window");
+                frame.setMinimumSize(new Dimension(600,400));
 
-        JMenuBar mBar = new JMenuBar();
+                JMenuBar mBar = new JMenuBar();
 
-        JMenu file = new JMenu("File");
-        JMenu close = new JMenu("Close");
+                JMenu file = new JMenu("File");
+                JMenu close = new JMenu("Close");
 
-        JMenuItem import_file = new JMenuItem("Import file");
-        JMenuItem export_file = new JMenuItem("Export file");
+                JMenuItem import_file = new JMenuItem("Import file");
+                JMenuItem export_file = new JMenuItem("Export file");
 
-        JMenuItem exit = new JMenuItem("Exit");
+                JMenuItem exit = new JMenuItem("Exit");
 
-        file.add(import_file);
-        file.add(export_file);
+                file.add(import_file);
+                file.add(export_file);
+                import_file.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        JFileChooser chooser = new JFileChooser();
+                        chooser.setMinimumSize(new Dimension(300,300));
+                        FileFilter filter = new FileNameExtensionFilter("PDF and DOCX","pdf","docx");
+                        chooser.setFileFilter(filter);
+                        chooser.showDialog(frame,"Import");
+                        pdfFile = chooser.getSelectedFile();
+                        String path = pdfFile.getAbsolutePath();
+                        System.out.println("Path:"+path);
+//                        PdfReader reader = new PdfReader(path);
 
-        close.add(exit);
+
+                    }
+                });
+
+                exit.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));                    }
+                });
+
+                close.add(exit);
 
 
-        mBar.add(file);
-        mBar.add(close);
+                mBar.add(file);
+                mBar.add(close);
 
-        frame.add(mBar,BorderLayout.PAGE_START);
+                frame.add(mBar,BorderLayout.PAGE_START);
 
-        //animation
-        MaterialUIMovement animate = new MaterialUIMovement( new Color(34,167,240),5,1000/30);
+                //animation
+                MaterialUIMovement animate = new MaterialUIMovement( new Color(34,167,240),5,1000/30);
 
-        animate.add(file);
-        animate.add(import_file);
-        animate.add(export_file);
+                animate.add(file);
+                animate.add(import_file);
+                animate.add(export_file);
 
-        animate.add(close);
-        animate.add(exit);
+                animate.add(close);
+                animate.add(exit);
 
-        frame.pack();
-        frame.setVisible(true);
+                frame.pack();
+                frame.setVisible(true);
+            }
+        });
     }
 }
