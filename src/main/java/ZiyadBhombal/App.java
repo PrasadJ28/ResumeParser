30ecc00e1508b06d29c74e5fa90b57c21260fecb
bhombalziyad20 <zeebhombal@gmail.com>
2018-02-20 17:34:15 +0530

    Ziyad-20-2-2018 Writes appropriate output to a text file.

--- src/main/java/ZiyadBhombal/App.java
+++ src/main/java/ZiyadBhombal/App.java
@@ -6,6 +6,7 @@ import com.itextpdf.text.pdf.parser.PdfTextExtractor;
 import com.itextpdf.text.pdf.*;
 import com.itextpdf.text.pdf.parser.SimpleTextExtractionStrategy;
 import mdlaf.*;
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 
 import javax.swing.*;
 import javax.swing.filechooser.FileFilter;
@@ -16,6 +17,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
@@ -73,7 +76,7 @@ public class App
                         chooser.showDialog(frame,"Import");
                         File importedFile = chooser.getSelectedFile();
                         if(importedFile.getName().contains(".pdf")){
-                            int result = processPdfDocument(frame,importedFile);
+                            int result = createFileDump(importedFile);
                         }
                         else if(importedFile.getName().contains(".docx")){
 
@@ -110,7 +113,7 @@ public class App
         });
     }
 
-    private static int processPdfDocument(JFrame frame,File f) {
+    private static int createFileDump(File f) {
 
         try{PdfReader reader = new PdfReader(f.getAbsolutePath());
             BufferedWriter writer = new BufferedWriter(new FileWriter("/home/ziyadbhombal/Documents/temp.txt",false));
@@ -121,8 +124,7 @@ public class App
                 line = PdfTextExtractor.getTextFromPage(reader,i, new SimpleTextExtractionStrategy());
                 String[] arr = line.split("\n");
                 for(String s : arr){
-                    writer.write(s+" ");
-                    writer.write("eof");
+                    writer.write(s);
                     writer.newLine();
                 }
             }
@@ -131,15 +133,130 @@ public class App
             System.out.println("Finished writing to dump");
             File file = new File("/home/ziyadbhombal/Documents/temp.txt");
             System.out.println("Size of dump on disk: "+file.getTotalSpace());
-            //start analysing text
+
+            List<String> techSkills,personalDetails,qualifications;
 
 
-            BufferedReader bufferedReader = new BufferedReader(new FileReader("/home/ziyadbhombal/Documents/temp.txt"));
+            techSkills = findTechnicalSkills();
+            qualifications = findQualifications();
+            personalDetails = findPersonalDetails();
+
+
+            BufferedWriter newWriter = new BufferedWriter(new FileWriter("/home/ziyadbhombal/Documents/result.txt",false));
+            newWriter.write("PersonalDetails:");
+            newWriter.newLine();
+            for(String s : personalDetails){
+                newWriter.write(s);
+            }
+            newWriter.newLine();
+            newWriter.write("TechnicalSkills:");
+            newWriter.newLine();
+            for(String s : techSkills){
+                newWriter.write(s);
+            }
+            newWriter.newLine();
+            newWriter.write("EducationalQualifications:");
+            newWriter.newLine();
+            for(String s : qualifications){
+                newWriter.write(s);
+            }
+            newWriter.close();
+
+            return RESULT_OK;
 
         }
         catch (IOException ex){ex.printStackTrace();}
 
 
         return RESULT_FAILED;
+    }
+
+    private static List<String> findPersonalDetails() {
+        try {
+            String rawFile = new String(Files.readAllBytes(Paths.get("/home/ziyadbhombal/Documents/temp.txt")));
+            String parsePersonalDetailsFrom = rawFile.split("Personal Information")[1];
+            List<String> personalDetails = Arrays.asList(parsePersonalDetailsFrom.split("\\s|\\n"));
+
+            for(String s: personalDetails){
+                System.out.println("Has details:"+s);
+            }
+            return personalDetails;
+        }
+        catch (IOException ex){
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+    private static List<String> findQualifications() {
+        try{
+            String rawFile = new String(Files.readAllBytes(Paths.get("/home/ziyadbhombal/Documents/temp.txt")));
+            String parseQualificationsFrom = rawFile.split("Educational Qualifications")[1];
+            String[] strArray = parseQualificationsFrom.split("[\\s\\n]");
+            List<String> educationalQualifications = new ArrayList<>();
+
+            int captureIndex=99;
+            for(int ix=0;ix<strArray.length;ix++){
+                if(strArray[ix].matches("Project :|Project")){
+                    captureIndex=ix;
+                }
+            }
+
+            for (int ix=0;ix<captureIndex;ix++){
+                educationalQualifications.add(strArray[ix]);
+            }
+
+            captureIndex = educationalQualifications.indexOf("Project");
+            System.out.println(captureIndex);
+
+            educationalQualifications = educationalQualifications.subList(0,captureIndex);
+
+            for (String s : educationalQualifications){
+                System.out.println("Has edu: "+s);
+            }
+
+            return educationalQualifications;
+
+
+        }
+        catch (IOException ex){
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+    private static List<String> findTechnicalSkills() {
+        try{
+            String rawFile = new String(Files.readAllBytes(Paths.get("/home/ziyadbhombal/Documents/temp.txt")));
+            String parseTechSkillFrom = rawFile.split("Technical Skills")[1];
+            String[] strArray = parseTechSkillFrom.split("[\\s\\n]");
+            List<String> technicalSkills= new ArrayList<>();
+            int caputeIndex=99;
+            for(int ix = 0; ix<strArray.length;ix++){
+                if (strArray[ix].contains("Extra-Curricular")){
+                    caputeIndex=ix;
+
+                }
+            }
+
+            for (int ix = 0;ix<caputeIndex;ix++){
+                if(!strArray[ix].equals("\\s")||!strArray[ix].equals("")){
+                    technicalSkills.add(strArray[ix]);
+                }
+
+            }
+
+            for (String s : technicalSkills){
+                System.out.println("Has: "+s);
+            }
+            return technicalSkills;
+
+
+
+        }
+        catch (IOException ex){
+            ex.printStackTrace();
+            return null;
+        }
     }
 }
